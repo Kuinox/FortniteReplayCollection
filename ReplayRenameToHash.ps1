d0d541839a02f5cde22a294c36d45bcc31c3fc36
Kuinox <nvandeginste@gmail.com>
2018-11-23 19:18:05 +0100

    First replays and a script to rename them properly

---
+++ ReplayRenameToHash.ps1
@@ -0,0 +1,5 @@
+﻿Get-ChildItem *.replay | ForEach-Object {
+    $Hash = Get-FileHash -Path $_.FullName -Algorithm SHA1
+    Write-Host "$($Hash.Hash).replay"
+    Rename-Item -Path $_.FullName -NewName "$($Hash.Hash).replay"
+}
