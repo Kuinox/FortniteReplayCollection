a12a6b00cc2ee55c1fce97d05cc078198b18cce4
ferron <ferron.saan@live.nl>
2018-11-24 11:11:57 +0100

    Add replays and Git LFS

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fortnite Replay Collection
+
+This is an initiative to collect Fortnite replays in order to track changes over time and test implementations of replay parsers.
+
+## Contribute your replays
+* Add your replay files to the _upload folder
+* Run the `ReplayRenameToHash` script
+```bash
+.\ReplayRenameToHash.ps1
+```
+* Make a Pull Request!
--- ReplayRenameToHash.ps1
+++ ReplayRenameToHash.ps1
@@ -1,5 +1,27 @@
-﻿Get-ChildItem *.replay | ForEach-Object {
-    $Hash = Get-FileHash -Path $_.FullName -Algorithm SHA1
-    Write-Host "$($Hash.Hash).replay"
-    Rename-Item -Path $_.FullName -NewName "$($Hash.Hash).replay"
+﻿# Get the replays files which should be added to Git
+$files = Get-ChildItem '.\_upload\*.replay'
+
+# Source location of the replays
+$targetPath = '.\Replays'
+
+foreach ($file in $files) {
+    $year = $file.LastWriteTime.Year.ToString()
+    $month = $file.LastWriteTime.Month.ToString()
+
+    $directory = $targetPath + "\" + $year + "\" + $month
+
+    # Create directory if it doesn't exsist
+    if (!(Test-Path $directory))
+    {
+        New-Item $directory -type directory
+    }
+
+    # $Hash = Get-FileHash -Path $file -Algorithm SHA1
+    # Write-Host "$($Hash.Hash).replay"
+    # Rename-Item -Path $file -NewName "$($Hash.Hash).replay"
+
+    # Move File to new location
+    # "./_upload/$($Hash.Hash).replay" | Move-Item -Destination $directory
+    $file | Move-Item -Destination $directory
+
 }
